2b28749206bd8339f93e7659c23e600496f0e481
jalilov08 <jalilovnozimjon054@gmail.com>
2024-04-20 21:30:53 +0500

    Uy ishi

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+let ism = prompt('Ismingizni kiriting:');
+let birinchiHarf = ism.charAt().toUpperCase();
+let davomi = ism.slice(1).toLocaleLowerCase();
+let res = birinchiHarf + davomi;
+
+alert(`Salom ${res}`);
+ */
